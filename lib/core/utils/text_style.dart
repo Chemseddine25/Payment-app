7a22a682024ddb5fe8871ba81440ea47196cfbd4
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-03-09 17:11:39 +0100

    feat : class text styles

---
+++ lib/core/utils/text_style.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+abstract class TextStyles {
+  static const TextStyle medium25 = TextStyle(
+    color: Colors.black,
+    fontSize: 25,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle semiBold24 = TextStyle(
+    color: Colors.black,
+    fontSize: 24,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle medium22 = TextStyle(
+    color: Colors.black,
+    fontSize: 22,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle regular18 = TextStyle(
+    color: Colors.black,
+    fontSize: 18,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semiBold18 = TextStyle(
+    color: Colors.black,
+    fontSize: 18,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+  );
+}
