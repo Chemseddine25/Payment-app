a9c65b193976f401b81cee240d8940f3bde58591
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-03-10 17:46:09 +0100

    feat: cart view body

---
+++ lib/features/checkout/presenation/views/widgets/cart_view_body.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:payment_app/core/utils/app_image.dart';
+import 'package:payment_app/core/utils/text_style.dart';
+import 'package:payment_app/features/checkout/presenation/views/payment_details_view.dart';
+import 'package:payment_app/features/checkout/presenation/views/widgets/custom_button.dart';
+
+class CartViewBody extends StatelessWidget {
+  const CartViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 26,
+          ),
+          AspectRatio(
+            aspectRatio: 424 / 277,
+            child: Image.asset(
+              Assets.assetsImagesCart,
+              fit: BoxFit.contain,
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          const OrderItemInfo(
+            title: ' Order Subtotal',
+            price: ' 42.97',
+          ),
+          const OrderItemInfo(
+            title: ' Discount',
+            price: ' 0',
+          ),
+          const OrderItemInfo(
+            title: ' Shipping',
+            price: ' 8',
+          ),
+          const SizedBox(
+            height: 17,
+          ),
+          const CustomDevider(),
+          const SizedBox(
+            height: 15,
+          ),
+          const TotalPrice(price: '50.97'),
+          const SizedBox(
+            height: 16,
+          ),
+          CustonButton(
+            onTap: () {
+              Navigator.pushReplacementNamed(
+                  context, PaymentDetailsView.routeName);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OrderItemInfo extends StatelessWidget {
+  const OrderItemInfo({
+    super.key,
+    required this.title,
+    required this.price,
+  });
+  final String title;
+  final String price;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: TextStyles.regular18,
+        ),
+        const Spacer(),
+        Text(
+          '\$$price',
+          textAlign: TextAlign.center,
+          style: TextStyles.regular18,
+        ),
+      ],
+    );
+  }
+}
+
+class CustomDevider extends StatelessWidget {
+  const CustomDevider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: const ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            width: 2,
+            strokeAlign: BorderSide.strokeAlignCenter,
+            color: Color(0xFFC6C6C6),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TotalPrice extends StatelessWidget {
+  const TotalPrice({super.key, required this.price});
+  final String price;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      const Text('Total',
+          textAlign: TextAlign.center, style: TextStyles.semiBold24),
+      const Spacer(),
+      Text(
+        ' \$$price',
+        textAlign: TextAlign.center,
+        style: TextStyles.semiBold24,
+      ),
+    ]);
+  }
+}
