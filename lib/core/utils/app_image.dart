aa6a5d1efebb9b447ce0412762148f9debb7cede
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-03-10 17:45:16 +0100

    feat: upade calss app image

--- lib/core/utils/app_image.dart
+++ lib/core/utils/app_image.dart
@@ -1,6 +1,25 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
-
+  
+  /// Assets for assetsImagesArrow
+  /// assets/images/Arrow.svg
   static const String assetsImagesArrow = "assets/images/Arrow.svg";
+
+  /// Assets for assetsImagesCart
+  /// assets/images/cart.png
+  static const String assetsImagesCart = "assets/images/cart.png";
+
+  /// Assets for assetsImagesPay
+  /// assets/images/pay.svg
+  static const String assetsImagesPay = "assets/images/pay.svg";
+
+  /// Assets for assetsImagesPaypal
+  /// assets/images/paypal.svg
+  static const String assetsImagesPaypal = "assets/images/paypal.svg";
+
+  /// Assets for assetsImagesVisa
+  /// assets/images/visa.svg
+  static const String assetsImagesVisa = "assets/images/visa.svg";
 }
+
