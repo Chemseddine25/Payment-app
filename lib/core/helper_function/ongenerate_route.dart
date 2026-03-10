446d46a096287e36889e91ec91de02cf9f05ee9a
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-03-10 17:44:47 +0100

    feat: on gernate route

--- lib/core/helper_function/ongenerate_route.dart
+++ lib/core/helper_function/ongenerate_route.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:payment_app/features/checkout/presenation/views/cart_view.dart';
+import 'package:payment_app/features/checkout/presenation/views/payment_details_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case CartView.routeName:
       return MaterialPageRoute(
         builder: (context) => const CartView(),
+      );
+
+    case PaymentDetailsView.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const PaymentDetailsView(),
       );
 
     default:
